5bc5ce337fd61658f740137e93fa6493d28c8903
WCS19 <wcs@bu.edu>
2025-01-03 22:35:51 -0700

    adding release description to build

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="sharepycrud",
     version="0.1.0",
     author="Willem Seethaler",
     author_email="wcs@bu.edu",
     description="Python library for CRUD operations on SharePoint",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
