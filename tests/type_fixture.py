d515f383bb3d0fae1375b7d893b5d90ed786b476
WCS19 <wcs@bu.edu>
2025-01-08 01:02:07 -0700

    Adding mypy args

--- tests/type_fixture.py
+++ tests/type_fixture.py
@@ -1,8 +1,8 @@
-from typing import TypeVar, Callable, cast
+from typing import TypeVar, Callable
 import pytest
 
 T = TypeVar("T")
 
 
 def typed_fixture(func: Callable[..., T]) -> Callable[..., T]:
-    return cast(Callable[..., T], pytest.fixture(func))
+    return pytest.fixture(func)
