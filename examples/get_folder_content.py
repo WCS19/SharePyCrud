46af80d84693bc5fb4286fde87ad26a7797b73ac
WCS19 <wcs@bu.edu>
2025-01-13 09:35:33 -0700

    Adding additional examples for client methods

--- examples/get_folder_content.py
+++ examples/get_folder_content.py
@@ -13,24 +13,20 @@ def main() -> None:
         print("Failed to get site ID")
         return
 
-    print(f"\nSite ID: {site_id}")
-
-    # List drives and root contents
-    drives = client.list_drives(site_id)
-    if not drives:
-        print("No drives found")
-
-    drive_name = "Documents"
-    drive_id = client.get_drive_id(site_id, drive_name)
+    drive_id = client.get_drive_id(site_id, "Documents")
+    print(f"Drive ID: {drive_id}")
     if not drive_id:
         print("Failed to get drive ID")
         return
 
-    parent_folders = client.list_parent_folders(drive_id=drive_id)
-    print(f"\nParent folders: {parent_folders}")
-    if parent_folders is not None:
-        for folder in parent_folders:
-            print(f"Folder: {folder['name']} (ID: {folder['path']})")
+    folder_id = client.get_folder_id(drive_id, "Folder1Test")
+    print(f"Folder ID: {folder_id}")
+    if not folder_id:
+        print("Failed to get folder ID")
+        return
+
+    folder_content = client.get_folder_content(drive_id=drive_id, folder_id=folder_id)
+    print(f"Folder content: {folder_content}")
 
 
 if __name__ == "__main__":
@@ -38,4 +34,3 @@ if __name__ == "__main__":
         main()
     except Exception as e:
         print(f"An error occurred: {e}")
-        raise
